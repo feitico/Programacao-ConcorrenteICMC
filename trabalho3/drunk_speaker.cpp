d136670b06914e9312dcf6312013a1cc7d6eb260
David César <dvd@localhost.localdomain>
2013-06-04 00:11:08 -0300

    trabalho3

--- trabalho3/drunk_speaker.cpp
+++ trabalho3/drunk_speaker.cpp
@@ -4,6 +4,8 @@
 #include <cstring>
 #include <string>
 #include <map>
+#include <iostream>
+#include <set>
 
 #define FILE_SIZE 24823019
 using namespace std;
@@ -14,9 +16,20 @@ void tic();
 double toc();
 
 // Read the whole file
-char words[FILE_SIZE];
+char* words; // [FILE_SIZE];
 
-map<string, int> dict;
+struct ltstr
+{
+  bool operator()(const char* s1, const char* s2) const
+  {
+    return strcmp(s1, s2) < 0;
+  }
+};
+
+set<char*, ltstr> dict;
+set<char*, ltstr> achadas;
+
+char* palavra_random();
 
 int main(int argc, char* argv[]) {
 	size_t total;
@@ -24,13 +37,65 @@ int main(int argc, char* argv[]) {
 	FILE *arq_palavras;
 	int i, begin_word;
 	char* temp;
-	
+	char* pch;
+	int dict_size;
+
 	tic();
 	arq_palavras = fopen(argv[1], "r");
 
-	total = fread(words, 1, FILE_SIZE, arq_palavras);
+	fseek(arq_palavras, 0L, SEEK_END);
+	int sz = ftell(arq_palavras);
+
+	fseek(arq_palavras, 0L, SEEK_SET);	
+
+	words = (char*) malloc (sizeof(char) * sz);
+	total = fread(words, 1, sz, arq_palavras);
 	
 	// Parsea as palavras lidas
+	pch = strtok (words, " ,\n");
+
+	for(int i=0; i<100; i++) {
+	//while(pch != NULL){
+		//printf ("%s\n",pch);
+		dict.insert(pch);
+		pch = strtok (NULL, " ,\n");
+		//getchar();
+
+	}
+	dict_size = dict.size();
+	set<char*>::iterator it;
+	
+	cout << dict_size << endl;
+
+	int k=0;
+	while(true) {
+		char * new_word = palavra_random();
+		it = dict.find(new_word);
+
+		if(it != dict.end()) {
+			cout << "encontrou: " << new_word << endl;
+			achadas.insert(*it);
+			dict.erase(it);
+			cout << achadas.size() << endl;
+			k++;
+			if(k == 2)
+				break;
+		} else {
+			cout << "nao: " << new_word << endl;
+		}
+	}	
+
+/*
+	for(it = dict.begin(); it != dict.end(); ++it){
+		cout << "a: " << *it << endl;
+	}
+*/		
+	for(it = achadas.begin(); it != achadas.end(); ++it){
+		cout << "achei: " << *it << endl;
+	}
+
+	// Parsea as palavras lidas
+	/*
 	begin_word = 0;
 	for(i=0; i<FILE_SIZE; i++) {
 		if( (words[i] == ',') ||
@@ -49,10 +114,12 @@ int main(int argc, char* argv[]) {
 	for(it=dict.begin(); it!=dict.end(); ++it) {
 		printf("%s %d\n", it->first.c_str(), it->second);
 	}
-
+*/
 	fclose(arq_palavras);
 	elapsed_time = toc();
 	printf("elapsed time: %f\ntotal %d\n", elapsed_time, (int)total);
+
+	free(words);
 
 	return 0;
 }
@@ -66,4 +133,19 @@ void tic() {
 double toc() {
 	clock_t end = clock();
 	return double(end - begin) / CLOCKS_PER_SEC;
+}
+
+char* palavra_random() {
+	char* str;
+	int tamanhoPalavra;
+
+	tamanhoPalavra = rand() % 5 + 1;
+	str = (char*) malloc(sizeof(char) * (tamanhoPalavra+1));
+	
+	for(int i = 0; i < tamanhoPalavra; i++){
+		str[i] = ('a' + rand() % 26);
+	}
+	str[tamanhoPalavra] = '\0';
+	
+	return str;
 }
